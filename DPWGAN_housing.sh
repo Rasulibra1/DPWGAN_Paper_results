4bf5f4f22ce86b43464b6a210f35d94a82469625
Rasul Ibragimov <ibragimr@uci.edu>
2026-01-24 23:53:37 -0800

    Create DPWGAN_housing.sh

---
+++ DPWGAN_housing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=gan_wine
+#SBATCH --partition=standard          # <-- change to your CPU partition name
+#SBATCH --account=AKENNEY1_LAB                # <-- your real account
+#SBATCH --nodes=1
+#SBATCH --ntasks=1                   # 1 task
+#SBATCH --cpus-per-task=4            # e.g. 4 CPU cores (adjust as needed)
+#SBATCH --time=72:00:00
+#SBATCH --mem=128G
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ibragimr@uci.edu
+
+module purge
+module load python/3.10.2
+#cd /data/homezvol2/ibragimr/paper-results-vinecops
+source .venv/bin/activate
+#python -m pip install --upgrade pip
+#python -m pip install statsmodels
+python DPWGAN_housing.py
