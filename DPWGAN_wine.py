9b4d66dd3d2a02d18e0b02f3f07a0b56db356c61
Rasul Ibragimov <ibragimr@uci.edu>
2026-01-28 15:47:31 -0800

    Update DPWGAN_wine.py

--- DPWGAN_wine.py
+++ DPWGAN_wine.py
@@ -23,7 +23,7 @@ from dpwgan.utils import create_categorical_gan
 ROOT = Path("/data/homezvol2/ibragimr/DPWGAN_Paper_results")
 
 SAVE_SYNTH = True
-SYNTH_DIR  = ROOT / "data" / "synth" / "wine" / "dpwgan"  # pick whatever path you like
+SYNTH_DIR  = ROOT / "data" / "synth" / "wine" / "dpwgan_ana"  # pick whatever path you like
 
 
 # You said train/holdout are in a folder called "data"
@@ -34,8 +34,8 @@ HOLDOUT_CSV = ROOT / "data" / "wine_holdout_final.csv"
 EVAL_DIR    = ROOT / "eval" / "metrics_raw" / "wine" / "dpwgan"
 SUMMARY_DIR = ROOT / "eval" / "summaries"
 
-EPS_LIST   = [1, 2, 3, 4, 5]
-SEED_RANGE = range(10, 20)
+EPS_LIST   = [5]
+SEED_RANGE = range(1, 21)
 
 # GAN hyperparams
 NOISE_DIM   = 20
